2f4fd82ef5406f1dd07d65ec066b338a8dbedbec
Victor Peña <victor.hp.mtz@gmail.com>
2020-07-09 21:18:13 -0500

    se creo tabla de inventario de productos, se inicia con formularios reactivos

--- src/app/admin/components/products-list/products-list.component.ts
+++ src/app/admin/components/products-list/products-list.component.ts
@@ -1,4 +1,9 @@
+import { Product } from './../../../core/models/product.model';
 import { Component, OnInit } from '@angular/core';
+
+// inyeccion de dependencias (inyectamos el servicio que podemos crear editar eliminas)
+// y lo inyectamos como una variable privada en el constructor
+import { ProductsService } from './../../../core/services/products/products.service';
 
 @Component({
   selector: 'app-products-list',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
+  // inicializamos el array de productos en vacio
+  products = [];
+  // orden de las columas para la tabla
+  displayedColumns: string[] = ['id', 'title', 'price', 'actions'];
 
-  constructor() { }
+  constructor(
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit() {
+    // ejecutamso el metodo this.fetchProducts para que se ejecute un request en network
+    this.fetchProducts();
   }
 
+
+  // necsitamos tener un metodos que nos traiga todos estosy datos ( productos que nos da el API)
+  // y este metodo se encarga de todos esto
+    fetchProducts() {
+      // obtenemos todos los productos
+      this.productsService.getAllProducts()
+      // y nos subscribimos porque es un obserbable
+      // como recibimos los productos ,
+      .subscribe(products => {
+        // le pasamos los productos de nuestra API a nuestro array de productos
+        this.products = products;
+      });
+    }
+
+    // creamos el metodo que utilizaran los botones
+    deleteProduct(id: string) {
+      // le decimos al this.productsService que quiero elimnar un producto le mandamos el id que quiero elimindar
+      this.productsService.deleteProduct(id)
+      // me suscribo para ver que se ejecute correctamente
+      .subscribe(rta => {
+        // imprimimos la respuesta rta para ver si lo elimino bien o mal
+        // console.log('respuesta', rta);
+        // mandamos un this.fetchProducts para que la tabla se actualice al momento de borrar
+        this.fetchProducts();
+      });
+    }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutModule } from '@angular/cdk/layout';
 
 // material module desde la documentacion lo importamos y exportamos
+import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
 // de admin.module
 import { MatInputModule } from '@angular/material/input';
-// import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
-// import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSortModule } from '@angular/material/sort';
