f1dfd41e796367a9aefd26d58d3041b6882ecc0f
Victor Peña <victor.hp.mtz@gmail.com>
2020-07-13 23:28:07 -0500

    card con pipe

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -4,6 +4,10 @@ import { Component, OnInit } from '@angular/core';
 // 2.1  y lo agregamos a nuestro constructor como una variable privada
 import { CartService } from './../../../core/services/cart.service';
 
+// 4.1 imprtamos map para transformar y para utilizar el pipe en el carrito
+import { map } from 'rxjs/operators';
+import { pipe } from 'rxjs';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -23,13 +27,31 @@ export class HeaderComponent implements OnInit {
     // cart$ => { observable del carrito
     // .subscribe hacemos una subscripcion
     // (products => y obtenemos todos lo productos que vallan agregando al carrrito
-    this.cartService.cart$.subscribe(products => {
+    // this.cartService.cart$.subscribe(products => {
       // imprimimos los prodcutos que vallan llegando pero necesitamos el tamaño
       // 3 creamos una varibale total en el expot
-      console.log(products);
-      // 3.1 una vez creada una ves que cambie total = al tamaño de productos que halla en el carro
-      this.total = products.length;
+    //   console.log(products);
+      // 3.1 una vez creada una ves que cambie total = al tamaño de productos que haya en el carro
+    //   this.total = products.length;
       // 3.1 y ahora la puedo reemplazan en nuestra variable matBadge en el html del header [matBadge]="total" para hacerlo dinamicante
+    //   //
+    //   //
+      // 4 ahora lo haremos con pipe ya que los flujos de datos se pueden preprocesar con un pipe
+      // 4.2
+    this.cartService.cart$
+    // agregamos la instruccion pipe
+    .pipe(
+      // agregamos el pipe map
+      // (products => y va a obtener los productos
+      // => products.length) y aqui sentencio como los va a transformar
+      // map(products => products.length) es una lista de productos y lo quiero transformar
+      //  a un solo valor el cual es el tamaño de esa lista
+      map(products => products.length)
+    )
+    // al colocar este valos en el subscibe ya no nos va a llegar la lista si no el tamaño entonces nos llega el total llega transfomado
+    .subscribe(total => {
+      // llega transformado
+      this.total = total;
     });
   }
 
